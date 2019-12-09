2d90f54eecb500a062294e8ed388f56a48989351
anthonysea <anthonyqchao@gmail.com>
2019-12-09 15:03:22 -0800

    Completed day1: part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,10 +1,16 @@
 #!/usr/bin python
 
 total_fuel_required = 0
+fuel_array = []
 
 with open('day1input.txt', 'r') as f:
     for mass in f.readlines():
-        fuel_required = (int(mass) // 3) - 2
-        total_fuel_required += fuel_required
+        fuel = (int(mass) // 3) - 2
+        while fuel > 0:
+            fuel_array.append(fuel)
+            fuel = fuel // 3 - 2
+        total_fuel_required += sum(fuel_array)
+        fuel_array.clear()
 
+print(fuel_array)
 print(total_fuel_required)
